d8a1b49857716157a9e4b205dbf23afebecd9352
Aru <aru24.m@gmail.com>
2024-09-03 12:15:20 -0400

    New Scripts

---
+++ main.py
@@ -0,0 +1,14 @@
+#print('Hello World')
+
+#print('Enter a todo:')
+#user_text = input("Enter a todo:")
+#print(f"Printing User Text - {user_text}")
+user_prompt = "Enter a todo:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1,todo2,todo3]
+#print(todos)
+
+print(type(todos))
